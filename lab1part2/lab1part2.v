9d0e78911706d8b2f02e25a35c118d4fbde3a7df
yangl56 <yangl56@mcmaster.ca>
2020-01-26 12:48:41 -0500

    part1 verilog file added

--- lab1part2/lab1part2.v
+++ lab1part2/lab1part2.v
@@ -5,25 +5,24 @@ module seven_seg_decoder(input [3:0] SW, output [6:0] HEX0);
 
 	assign HEX0[6:2]=reg_LEDs[6:2];
 
-	always @(*) 
-	begin	
+	always @(*) begin	
 		case (SW) 
-			4’b0000: reg_LEDs[6:2]=5’b10000;
-			4’b0001: reg_LEDs[6:2]=5’b11110;
-			4’b0010: reg_LEDs[6:2]=5’b01001;
-			4’b0011: reg_LEDs[6:2]=5’b01100;
-			4’b0100: reg_LEDs[6:2]=5’b00110;
-			4’b0101: reg_LEDs[6:2]=5’b00100;
-			4’b0110: reg_LEDs[6:2]=5’b00000;
-			4’b0111: reg_LEDs[6:2]=5’b11110;
-			4’b1000: reg_LEDs[6:2]=5’b00000;
-			4’b1001: reg_LEDs[6:2]=5’b00100;
-			4’b1010: reg_LEDs[6:2]=5’b01110;
-			4’b1011: reg_LEDs[6:2]=5’b00010;
-			4’b1100: reg_LEDs[6:2]=5’b10000;
-			4’b1101: reg_LEDs[6:2]=5’b10011;
-			4’b1110: reg_LEDs[6:2]=5’b00010;
-			4’b1111: reg_LEDs[6:2]=5’b11111;
-		endcase
-	end 
-endmodule 
+			4’b0000: reg_LEDs[6:2]<=5’b10000;
+			4’b0001: reg_LEDs[6:2]<=5’b11110;
+			4’b0010: reg_LEDs[6:2]<=5’b01001;
+			4’b0011: reg_LEDs[6:2]<=5’b01100;
+			4’b0100: reg_LEDs[6:2]<=5’b00110;
+			4’b0101: reg_LEDs[6:2]<=5’b00100;
+			4’b0110: reg_LEDs[6:2]<=5’b00000;
+			4’b0111: reg_LEDs[6:2]<=5’b11110;
+			4’b1000: reg_LEDs[6:2]<=5’b00000;
+			4’b1001: reg_LEDs[6:2]<=5’b00100;
+			4’b1010: reg_LEDs[6:2]<=5’b01110;
+			4’b1011: reg_LEDs[6:2]<=5’b00010;
+			4’b1100: reg_LEDs[6:2]<=5’b10000;
+			4’b1101: reg_LEDs[6:2]<=5’b10011;
+			4’b1110: reg_LEDs[6:2]<=5’b00010;
+			4’b1111: reg_LEDs[6:2]<=5’b11111;
+		end case
+	end
+end module
