16dc20aebeca44753137bdbd8537e603191fd730
Adam Bujak <dmbujak3@gmail.com>
2020-02-09 13:25:41 -0500

    added game logic

--- 2lab/lab2.v
+++ 2lab/lab2.v
@@ -41,170 +41,266 @@ module random (input clk, reset_n, resume_n, output reg [13:0] random,
 				reg_values[1]=reg_values[2];
 				reg_values[0]=reg_values[0] ^ reg_values[1];
 				// tap 1 of the diagram from the lab manual
-				random = reg_values + 1'd1000;
-				if (random > 1'd5000)
+				random = reg_values + 14'd1000;
+				if (random > 14'd5000)
 				begin
-					random = reg_values % 1'd5000;
+					random = reg_values % 14'd5000;
 				end
 			end //end of ENABLE.
 		end
 	end
 endmodule
 
-module hex_to_bcd_converter(input wire clk, reset, input wire [19:0] hex_number, 
-output [3:0] bcd_digit_0,bcd_digit_1,bcd_digit_2,bcd_digit_3, bcd_digit_4, 
-bcd_digit_5);
-	// DE1-SoC has 6 7_seg_LEDs, 20 bits can represent decimal 999999.
-	//This module is designed to convert a 20 bit binary representation
-	//to BCD
-	
-	integer i, k;
-	wire [19:0] hex_number1; // the last 20 bits of input hex_number
-	reg [3:0] bcd_digit [5:0]; //simplifies program
-	
-	assign hex_number1=hex_number[19:0];
-	assign bcd_digit_0=bcd_digit[0];
-	assign bcd_digit_1=bcd_digit[1];
-	assign bcd_digit_2=bcd_digit[2];
-	assign bcd_digit_3=bcd_digit[3];
-	assign bcd_digit_4=bcd_digit[4];
-	assign bcd_digit_5=bcd_digit[5];
-	always @ (*)
-		begin
-		//set all 6 digits to 0
-		bcd_digit[0] <= 1'b0;
-		bcd_digit[1] <= 1'b0;
-		bcd_digit[2] <= 1'b0;
-		bcd_digit[3] <= 1'b0;
-		bcd_digit[4] <= 1'b0;
-		bcd_digit[5] <= 1'b0;
-		bcd_digit[6] <= 1'b0;
-		//shift 20 times
-		bcd_digit[0] = bcd_digit[0] << 20;
-		bcd_digit[1] = bcd_digit[0] << 20;
-		bcd_digit[2] = bcd_digit[0] << 20;
-		bcd_digit[3] = bcd_digit[0] << 20;
-		bcd_digit[4] = bcd_digit[0] << 20;
-		bcd_digit[5] = bcd_digit[0] << 20;
-			for (i=19; i>=0; i=i-1)
-			begin
-				//check all 6 BCD tetrads, if >=5 then add 3
-				for (k=5; k>=0; k=k-1)
-				begin
-				/* fill your code here */
-				end
-				
-				//shift one bit of BIN/HEX left
-				//for the first 5 tetrads
-				for (k=5; k>=1; k=k-1)
-				begin
-				bcd_digit[k]=bcd_digit[k] << 1;
-				bcd_digit[k][0]=bcd_digit[k-1][3];
-				end
-				//shift one bit of BIN/HEX left, for the last tetrad
-				/* fill your code here */
-			end //end for loop
-		end //end of always.
-endmodule
 
-module clock_divider (input Clock, Reset_n, output reg clk_ms);
-	parameter factor=10; //50000; // 32’h000061a7;
-	reg [31:0] countQ;
-	
-	always @ (posedge Clock, negedge Reset_n)
-	begin
-		if (!Reset_n) 
-		begin
-		/* fill in your code here */
-		end
+
+
+//The requirements for your project:
+
+// 1.   	Key1 for reset, Key2 for resume, Key0 is player 1, Key3 is for player 2
+// 2.   	If Key0 is pressed earlier than Key3, player 1 wins. If Key3 is pressed earlier, player2 wins.
+// 3. 	If Key0 and Key3 pressed at the same time, no one wins. 
+// 4. 	If player1 wins, one more LEDs of the LED0-4 will light up.If player2 wins, one more LEDs of the LED=9-5 will light up,  
+// 5. 	After power up, or after reset, all of LEDs are off. the HEXs will blink for 5 seconds. then will be off for  2+randdom seconds.
+			//Here the random ranges form 1 sec to 5 sec
+// 6. 	The winner's reaction time will show by the HEXs.
+//	7. 	If there is a cheating (press the KEY0 or KEY3 before the timer starts,(in program, (set that if the timer reading is less than 
+			//80 ms, it is cheating)
+			//the cheater's number, either 111111 or 222222 will show by HEXs. The program then stop for resumeing for next round.
+//	8. 	if both player is cheating at the same time (or both player pressed at the same time, which is not likely to happen), display 888888 by HEXs and then wait to resume the game.
+
+
+
+`default_nettype none
+module lab2(input CLOCK_50,  input [3:0] KEY,  output [6:0] HEX0,HEX1,HEX2,HEX3,HEX4,HEX5, output [9:0] LEDR);
 		
+
+	parameter [2:0] RESET=3'b000, RESUME=3'b001, BLINKING=3'b010, OFF=3'b011, TIMER_DISPLAY=3'b100, WINNER_TIME_DISPLAY=3'b101,
+							WIN1=3'b110, WIN2=3'b111;
+	parameter PRESSED=1'b0, RELEASED=1'b1;
+	
+	reg [2:0] state=RESET, next_state=RESET;
+	
+	
+	wire clk_ms;
+	wire [19:0] ms, display_ms;
+	wire [3:0] w_ms0,w_ms1,w_ms2,w_ms3, w_ms4,w_ms5; //wires after hex_to_bcd_converter.v for displayed time
+	wire [3:0] w_blink0, w_blink1, w_blink2, w_blink3, w_blink4, w_blink5;  //wires afer  blinking 
+	wire [3:0] winner_ms0,winner_ms1,winner_ms2,winner_ms3, winner_ms4,winner_ms5; //wires afer hex_to_bcd_converter.v for winner time
+
+	
+	wire [3:0] digit0, digit1, digit2, digit3, digit4, digit5;  //wires afer mux.v
+	
+	
+	wire [13:0] random_wait_time;
+
+	
+	reg [1:0]  hex_sel=2'b00;  //whether blinking or not
+	wire [1:0] w_hex_sel; 
+	
+	
+	reg display_counter_start;
+	wire w_display_counter_start;
+	
+	
+	reg player1_win, player2_win;   // if is 0, not win, if 1 win,
+	
+	reg player1_state, player2_state;
+	
+	reg player1_cheating, player2_cheating;
+	
+	reg[4:0] win1=5'b00000, win2=5'b00000;   // score for player 1 and 2.
+
+	reg [19:0]temp;
+
+	reg [19:0] winner_time=8888;
+	wire [19:0] w_winner_time;
+	
+	wire conditioned_key0, conditioned_key3;
+
+	assign w_winner_time=winner_time;
+	
+	
+	
+	assign w_hex_sel=hex_sel;
+	
+	assign w_display_counter_start=display_counter_start; 
+	
+	assign LEDR[4:0]=win1;
+	assign LEDR[9:5]={win2[0],win2[1],win2[2],win2[3],win2[4]} ;   
+	
+	wire random_ready;
+
+	
+	clock_divider #(.factor(50000)) (.Clock(CLOCK_50), .Reset_n(KEY[1]), .Pulse_ms(clk_ms));
+	
+	counter (.clk(clk_ms), .reset_n(KEY[1]), .resume_n(KEY[2]), .enable(1), .ms_count(ms));
+
+	counter (.clk(clk_ms), .reset_n(KEY[1]), .resume_n(KEY[2]), .enable(w_display_counter_start), .ms_count(display_ms));
+	
+   random (.clk(clk_ms),  .reset_n(KEY[1]), .resume_n(KEY[2]), .random(random_wait_time), .rnd_ready(random_ready));
+	
+	blinkHEX #(.factor(200) ) (.ms_clk(clk_ms), .Reset_n(KEY[1]), .d0(w_blink0), .d1(w_blink1), .d2(w_blink2), .d3(w_blink3), .d4(w_blink4),.d5(w_blink5));
+	
+	
+
+	
+	
+	assign digit0=w_blink0;
+	assign digit1=w_blink1;
+	assign digit2=w_blink2;
+	assign digit3=w_blink3;
+	assign digit4=w_blink4;
+	assign digit5=w_blink5;
+	
+	
+	seven_seg_decoder  decoder0(digit0, HEX0);
+	seven_seg_decoder  decoder1(digit1, HEX1);
+	seven_seg_decoder  decoder2(digit2, HEX2);
+	seven_seg_decoder  decoder3(digit3, HEX3);
+	seven_seg_decoder  decoder4(digit4, HEX4);
+	seven_seg_decoder  decoder5(digit5, HEX5);
+
+	
+	always @ (posedge CLOCK_50, negedge KEY[1], negedge KEY[2])
+	begin
+		if (!KEY[1])    // reset
+			begin
+				state<=RESET;
+			end
+		else if (!KEY[2])   //start/resume
+			begin
+				state<=RESUME;
+			end
+	
 		else
-		begin
-			if (countQ<factor/2)
 			begin
-			/* fill in your code here */
+				state<=next_state;
 			end
+	end
+	
+
+	always @ (posedge CLOCK_50)
+	begin
+		player1_state <= KEY[0];
+		player2_state <= KEY[3];
+	end
+	
+	always @(posedge CLOCK_50, negedge KEY[1] )    //for solving the inferred latch problem caused by win1 and win2.
+	begin 
+		if (!KEY[1]) begin
+			win1<=5'b00000;
+			win2<=5'b00000;
+		end			
+		else if (player1_win==1)
+				win1<=(win1<<1) | 5'b00001;
+		else if (player2_win==1)
+				win2<=(win2<<1) | 5'b00001;
+		
+	end
+	
+		
+	
+	
+	always @ (*) 
+	begin
+		next_state=state;   //default
+		player1_win=0;
+		player2_win=0;
+		
+		
+		case (state)
+			RESET: 
+				begin					
+					display_counter_start=0;
+					winner_time=0;					
+									
+					hex_sel=2'b00;
+					next_state=BLINKING;			
+				end
+			RESUME:
+				begin					
+					//hex_sel=2'b00;
+					display_counter_start=0;
+					winner_time=0;
+				
+					hex_sel=2'b00;
+					next_state=BLINKING;
+				end
+			BLINKING:
+				begin
+					//hex_sel=2'b00;
+					
+	
+					if (ms>=5000)        //blink for  about 5 second	
+						begin
+							//hex_sel=2'b01;
+							next_state=OFF;
+						end
+					else 
+						next_state=BLINKING;
+						
+				end
 			
-			else if (countQ<factor)
-			begin
-			/* fill in your code here */
-			end
+			OFF:
+				begin
+					hex_sel=2'b01;
+					
+									
+					if (ms>(3000+random_wait_time)) begin     //(7-5) seconds + random seconds)
+						
+						next_state=TIMER_DISPLAY;	
+						
+					end
+					player1_cheating = !player1_state;
+					player2_cheating = !player2_state;
+				end
+			TIMER_DISPLAY:
+				begin
+					display_counter_start=1;  
+					hex_sel=2'b10;	
+					if ((player1_cheating && player2_cheating) || (player2_state == PRESSED && player2_state == PRESSED)) // display all 8
+					begin 
+					
+					end
+					else if (player1_cheating) // display all 1
+					begin 
+					
+					end
+					else if (player2_cheating) // display all 2
+					begin 
+					
+					end
+				
+					if (player1_state == PRESSED)
+					begin
+						player1_win=1;
+						winner_time = display_ms;
+						next_state=WINNER_TIME_DISPLAY;
+					end
+					else if (player2_state == PRESSED)
+					begin
+						player2_win=1;
+						winner_time = display_ms;
+						next_state=WINNER_TIME_DISPLAY;
+					end
+				end
+
+
+			WINNER_TIME_DISPLAY:
+				begin
+					hex_sel=2'b11;
+					winner_time=winner_time;
+					
+				end
 			
-			else //countQ==factor
-			begin
-			/* fill in your code here */
-			end
-		end
+			default: 
+				begin
+					next_state=RESET;
+				end
+		endcase	
+	
+	
 	end
-endmodule
-
-module seven_seg_decoder ( input [3:0] SW, output [6:0] HEX0 );
-	reg [6:0] reg_LEDs;
-	assign HEX0[0]= (SW[2]&(~SW[1])&(~SW[0]) | (~SW[3])&(~SW[2])&(~SW[1])&SW[0] | SW[3]&SW[2]&SW[1]&SW[0] | SW[3]&(~SW[2])&SW[1]&(~SW[0])); 
-	assign HEX0[1]= ((~SW[3])&SW[2]&(~SW[1])&SW[0] | (~SW[3])&SW[2]&SW[1]&(~SW[0]) | SW[3]&SW[2]&SW[1]&SW[0] | SW[3]&(~SW[2])&SW[1]&(~SW[0]));
-
-	assign HEX0[6:2]=reg_LEDs[6:2];
-
-	always @(*) begin	
-			case (SW) 
-				4'b0000: reg_LEDs[6:2]<=5'b10000;
-				4'b0001: reg_LEDs[6:2]<=5'b11110;
-				4'b0010: reg_LEDs[6:2]<=5'b01001;
-				4'b0011: reg_LEDs[6:2]<=5'b01100;
-				4'b0100: reg_LEDs[6:2]<=5'b00110; 
-				4'b0101: reg_LEDs[6:2]<=5'b00100; 
-				4'b0110: reg_LEDs[6:2]<=5'b00000; 
-				4'b0111: reg_LEDs[6:2]<=5'b11110;
-				4'b1000: reg_LEDs[6:2]<=5'b00000;
-				4'b1001: reg_LEDs[6:2]<=5'b00100;
-				4'b1010: reg_LEDs[6:2]<=5'b10001; 
-				4'b1011: reg_LEDs[6:2]<=5'b00010;
-				4'b1100: reg_LEDs[6:2]<=5'b10000; 
-				4'b1101: reg_LEDs[6:2]<=5'b10011;
-				4'b1110: reg_LEDs[6:2]<=5'b00010;
-				4'b1111: reg_LEDs[6:2]<=5'b11111; 
-			endcase
-	end
-endmodule
-
-module counter(input clk, reset_n, start_n, stop_n, output reg [WIDTH:0] ms_count);
-	parameter WIDTH=20;
 	
-	always @(posedge clk, negedge reset_n, negedge start_n)
-		if (!reset_n) //pressed button will send logic 0
-			ms_count<=20'b0;
-		else
-			ms_count<=ms_count+1'b1;
-endmodule
 
-module lab2tut (input CLOCK_50, input [2:0] KEY, output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, 
-output [9:0] LED0, LED1, LED2, LED3, LED4, LED5, LED6, LED7, LED8, LED9);
-	wire clk_en;
-	wire [19:0] ms;
-	wire [3:0]  digit0, digit1, digit2, digit3, digit4, digit5;
-	wire [13:0] random;
-	wire randomReady;
-	reg  [1:0]  state;
-	reg  resumeReg = 1'b1;
-	reg  resetReg  = 1'b1;
-	wire resume    = resumeReg;
-   wire reset     = resetReg;	
-	
-	random(.clk(clk_en), .reset(reset), .resume(resume), .random(random), .rnd_ready(randomReady));
-	clock_divider #(.factor(50000)) (.Clock(CLOCK_50), .Reset_n(KEY[0]), .Pulse_ms(clk_en));
-	counter(.clk(clk_en), .reset_n(KEY[0]), .start_n(KEY[1]), .stop_n(KEY[2]), .ms_count(ms));
-	
-	hex_to_bcd_converter(CLOCK_50, KEY[0], ms, digit0,digit1,digit2,digit3, digit4,digit5);
-	seven_seg_decoder decoder0(digit0, HEX0);
-	
-	// ToDo: create a timer block - generates a signal on timeout - uses counter to count to a certain number use parameter
-	always @(*) begin	
-		case (state) 
-			2'b00: reg_LEDs[6:2]<=5'b10000; // HEX LEDS blinking - Pregame
-			2'b01: reg_LEDs[6:2]<=5'b11110; // HEX LEDS off
-			2'b10: reg_LEDs[6:2]<=5'b01001; // HEX LEDS counting - game
-			2'b10: reg_LEDs[6:2]<=5'b01001; // 
-		endcase
-	end
-	/* fill in your code here */
 endmodule
