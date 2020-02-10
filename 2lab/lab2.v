49adf18b2763087e552c6cd825890209b4675cbc
yangl56 <yangl56@mcmaster.ca>
2020-02-10 17:04:13 -0500

    worked on optimizing RNG

--- 2lab/lab2.v
+++ 2lab/lab2.v
@@ -1,16 +1,76 @@
-module random (input clk, reset_n, resume_n, output reg [13:0] random, output reg rnd_ready);
+//module random (input clk, reset_n, resume_n, output reg [13:0] random, output reg rnd_ready);
+//	//for 14 bits Liner Feedback Shift Register,
+//	//the Taps that need to be XNORed are: 14, 5, 3, 1
+//	wire xnor_taps, and_allbits, feedback;
+//	
+//	reg [13:0] reg_values;
+//	reg enable=1;
+//	
+//	assign feedback = reg_values[0] ^ reg_values[2] ^ reg_values[4] ^ reg_values[13];
+//	
+//	initial
+//	begin
+//		reg_values=14'b11111111111111;
+//		//the LFSR cannot be all 0 at beginning.
+//		enable=1;
+//		rnd_ready=0;
+//	end
+//	always @ (posedge clk, negedge reset_n, negedge resume_n)
+//	begin
+//		if (!reset_n)
+//		begin
+//			reg_values=14'b11111111111111;
+//			//the LFSR cannot be all 0 at beginning.
+//			enable=1;
+//			rnd_ready=0;
+//		end
+//		else if (!resume_n)
+//		begin
+//			enable=1;
+//			rnd_ready=0;
+//			//reg_values=reg_values;
+//		end
+//		else
+//		begin
+//			if (enable)
+//			begin
+//				
+//				reg_values[13]=reg_values[0];
+//				reg_values[12:5]=reg_values[13:6];
+//				reg_values[4]=reg_values[0] ^ reg_values[5];
+//				// tap 5 of the diagram from the lab manual
+//				reg_values[3]=reg_values[4];
+//				reg_values[2]=reg_values[0] ^ reg_values[3];
+//				// tap 3 of the diagram from the lab manual
+//				reg_values[1]=reg_values[2];
+//				reg_values[0]=reg_values[0] ^ reg_values[1];
+//				//tap 1 of the diagram from the lab manual
+//				
+//				random = {reg_values[13:1], feedback};
+//				
+//				random = reg_values + 14'd1000;
+//				if (random > 14'd5000)
+//				begin
+//					random = reg_values % 14'd5000;
+//				end
+//			end //end of ENABLE.
+//		end
+//	end
+//endmodule
+
+module random (input clk, reset_n, resume_n, output reg [11:0] random, output reg rnd_ready);
 	//for 14 bits Liner Feedback Shift Register,
 	//the Taps that need to be XNORed are: 14, 5, 3, 1
 	wire xnor_taps, and_allbits, feedback;
 	
-	reg [13:0] reg_values;
+	reg [11:0] reg_values;
 	reg enable=1;
 	
-	assign feedback = reg_values[0] ^ reg_values[2] ^ reg_values[4] ^ reg_values[13];
+	assign feedback = reg_values[0] ^ reg_values[2] ^ reg_values[4] ^ reg_values[11];
 	
 	initial
 	begin
-		reg_values=14'b11111111111111;
+		reg_values=12'b111111111111;
 		//the LFSR cannot be all 0 at beginning.
 		enable=1;
 		rnd_ready=0;
@@ -19,7 +79,7 @@ module random (input clk, reset_n, resume_n, output reg [13:0] random, output re
 	begin
 		if (!reset_n)
 		begin
-			reg_values=14'b11111111111111;
+			reg_values=12'b111111111111;
 			//the LFSR cannot be all 0 at beginning.
 			enable=1;
 			rnd_ready=0;
@@ -35,8 +95,8 @@ module random (input clk, reset_n, resume_n, output reg [13:0] random, output re
 			if (enable)
 			begin
 				
-				reg_values[13]=reg_values[0];
-				reg_values[12:5]=reg_values[13:6];
+				reg_values[11]=reg_values[0];
+				reg_values[10:5]=reg_values[11:6];
 				reg_values[4]=reg_values[0] ^ reg_values[5];
 				// tap 5 of the diagram from the lab manual
 				reg_values[3]=reg_values[4];
@@ -46,13 +106,9 @@ module random (input clk, reset_n, resume_n, output reg [13:0] random, output re
 				reg_values[0]=reg_values[0] ^ reg_values[1];
 				//tap 1 of the diagram from the lab manual
 				
-				random = {reg_values[13:1], feedback};
+				random = {reg_values[11:1], feedback};
 				
-				random = reg_values + 14'd1000;
-				if (random > 14'd5000)
-				begin
-					random = reg_values % 14'd5000;
-				end
+				random = random + 12'd1000;
 			end //end of ENABLE.
 		end
 	end
